740ae3c5b226d94a2d64314d39dc95d097d76467
Weverton Bruno <oieusouweverton@hotmail.com>
2021-09-07 09:41:45 -0300

    feat: Project created

---
+++ src/main/java/dev/weverton/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.weverton.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/weverton/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.weverton.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
